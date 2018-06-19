9d9ba550de7cc426de6703b6691475cdada674a9
Arthur Antunes <arthuragds@gmail.com>
2018-06-18 22:12:31 -0300

    Adding part of the robot behaviour

--- src/Robot.java
+++ src/Robot.java
@@ -2,11 +2,21 @@ import eduni.simjava.*;
 
 public class Robot extends Sim_entity{
 	private Sim_port out;
+	private double delay;
 	
-	public Robot(String name) {
+	public Robot(String name, double delay) {
 		super(name);
+		this.delay = delay;
+		
 		out = new Sim_port("Out");
 		add_port(out);
 	}
 
+	public void body() {
+		for(int i = 0; i < 30; i++) {
+			sim_schedule(out, 0.0, 0);
+			sim_pause(delay);
+		}
+	}
+		
 }
